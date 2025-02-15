854d766b6f51e8a405e6fc669acfe67ed8057711
AAminha <dks4857@gmail.com>
2025-02-15 22:17:49 +0900

    chore: import 순서 정렬 lint 추가

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import prettierConfig from 'eslint-config-prettier';
 import pluginQuery from '@tanstack/eslint-plugin-query';
+import importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -24,11 +25,40 @@ export default tseslint.config(
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
       '@tanstack/query': pluginQuery,
+      import: importPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       '@tanstack/query/exhaustive-deps': 'error',
+      'import/order': [
+        'error',
+        {
+          groups: [
+            // 기본 모듈 -> 외부 모듈 -> 내부 모듈 -> 스타일링
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@/**',
+              group: 'internal',
+              position: 'after',
+            },
+          ],
+          'newlines-between': 'always', // 그룹 간에 새로운 줄을 추가하도록 설정
+          alphabetize: { order: 'asc', caseInsensitive: true }, // 알파벳 순으로 정렬
+        },
+      ],
     },
   },
 );
