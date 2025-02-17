05c137adde5081dab5c357c4bfccd70c758ae91b
AAminha <dks4857@gmail.com>
2025-02-17 23:33:56 +0900

    design: 로그인 페이지 에셋 추가

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,6 @@
+import GoogleIcon from './google.svg?react';
+import KakaoIcon from './kakao.svg?react';
+import NaverIcon from './naver.svg?react';
+import StampIcon from './stamp.svg?react';
+
+export { NaverIcon, KakaoIcon, GoogleIcon, StampIcon };
