7847831a17ae0b2c4d8b047eec07c78decc9ba76
AAminha <dks4857@gmail.com>
2025-02-18 22:44:36 +0900

    design: 편지 공유 게시판 퍼블리싱

--- src/pages/LetterBoard/components/LetterPreview.tsx
+++ src/pages/LetterBoard/components/LetterPreview.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router';
 
-import MemoBg from '@/assets/images/letter-pink.png';
+import PinkLetterBg from '@/assets/images/letter-pink.png';
 
 interface LetterPreviewProps {
   id: string;
@@ -14,7 +14,7 @@ const LetterPreview = ({ id, to, from, content }: LetterPreviewProps) => {
     <Link to={id}>
       <article
         className="caption-r flex flex-col gap-2 rounded-sm bg-[image:var(--bg-image)] bg-[length:100%_100%] bg-center px-3 py-2"
-        style={{ '--bg-image': `url(${MemoBg})` } as React.CSSProperties}
+        style={{ '--bg-image': `url(${PinkLetterBg})` } as React.CSSProperties}
       >
         <p>From.{from}</p>
         <p className="line-clamp-2 font-light">{content}</p>
--- src/pages/LetterBoard/index.tsx
+++ src/pages/LetterBoard/index.tsx
@@ -1,5 +1,34 @@
+import NoticeRolling from '@/components/NoticeRolling';
+import PageTitle from '@/components/PageTitle';
+import Header from '@/layouts/Header';
+
+import LetterPreview from './components/LetterPreview';
+
 const LetterBoardPage = () => {
-  return <div>LetterBoardPage</div>;
+  return (
+    <>
+      <Header />
+      <main className="mt-[-9px] flex grow flex-col items-center px-5 pb-10">
+        <NoticeRolling />
+        <PageTitle className="mt-4">게시판</PageTitle>
+        <p className="text-gray-60 caption-m mt-4.5">
+          따숨이에게 힘이 되었던 다양한 편지들을 모아두었어요
+        </p>
+        <section className="mt-6 grid grid-cols-2 gap-x-5 gap-y-4">
+          {Array.from({ length: 10 }).map((_, index) => (
+            <LetterPreview
+              key={index}
+              id={`${index}`}
+              to="12E21"
+              from="12E21"
+              content="저희가 주고 받은 행운의 편지 저희가 주고 받은 행운의 편지 저희가 주고 받은 행운의 편지
+        저희가 주고 받은 행운의 편지"
+            />
+          ))}
+        </section>
+      </main>
+    </>
+  );
 };
 
 export default LetterBoardPage;
