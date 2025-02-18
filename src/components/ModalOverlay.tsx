aad7fb9799869e9077375e8268c86488e9fc3687
AAminha <dks4857@gmail.com>
2025-02-19 02:11:22 +0900

    refactor: ModalOverlay 컴포넌트로 모달의 외부 영역 클릭 로직 이동

--- src/components/ModalOverlay.tsx
+++ src/components/ModalOverlay.tsx
@@ -1,11 +1,28 @@
 interface ModalOverlayProps {
+  closeOnOutsideClick?: boolean;
   children: React.ReactElement;
+  onClose?: () => void;
 }
 
-const ModalOverlay = ({ children }: ModalOverlayProps) => {
+const ModalOverlay = ({ closeOnOutsideClick = false, children, onClose }: ModalOverlayProps) => {
+  const handleClickOutside = () => {
+    if (closeOnOutsideClick && onClose) {
+      onClose();
+    }
+  };
+
+  const handleClickInside = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation();
+  };
+
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60">
-      {children}
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black/60"
+      onClick={handleClickOutside}
+    >
+      <div className="flex flex-col" onClick={handleClickInside}>
+        {children}
+      </div>
     </div>
   );
 };
