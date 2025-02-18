9f6bb6776d46ccfd9e69e6f90851810df8b982ea
Minha Ahn <dks4857@gmail.com>
2025-02-18 17:28:04 +0900

    design: 마이페이지 퍼블리싱 (#8)
    * design: 마이페이지 에셋 추가
    
    * design: 마이페이지 퍼블리싱
    
    * design: ConfirmModal 컴포넌트 구현 및 탈퇴 모달 적용
    
    * design: 모달 배경 이미지 적용
    
    * design: 모달 컴포넌트 버튼 비율 동일하도록 수정
    
    * refactor: 중복되는 버튼 디자인 스타일 클래스로 분리
    
    * design: 배경 오버레이 이미지 추가
    
    * fix: useViewport 계산 상수 수정
    
    * feat: 온도에 맞는 텍스트 보여주는 함수 추가
    
    * refactor: hn 태그 적용
    
    * refactor: Header 컴포넌트를 공통 레이아웃으로 분리
    
    * refactor: 모달 오버레이 공통 컴포넌트로 분리

--- src/hooks/useViewport.ts
+++ src/hooks/useViewport.ts
@@ -3,8 +3,8 @@ import { useEffect } from 'react';
 function useViewport() {
   useEffect(() => {
     const setViewport = () => {
-      const vh = window.innerHeight * 0.00999;
-      const vw = document.documentElement.clientWidth * 0.00999;
+      const vh = window.innerHeight * 0.01;
+      const vw = document.documentElement.clientWidth * 0.01;
       document.documentElement.style.setProperty('--vh', `${vh}px`);
       document.documentElement.style.setProperty('--vw', `${vw}px`);
     };
