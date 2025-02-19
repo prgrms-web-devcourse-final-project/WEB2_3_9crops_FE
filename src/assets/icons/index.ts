2562d7c4c3df6620f509dc3ba4191a259a2c09ec
안지원 <wl990@naver.com>
2025-02-20 08:55:12 +0900

    design:편지 상세보기 퍼블리싱

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -5,5 +5,23 @@ import EnvelopeIcon from './envelope.svg?react';
 import PersonIcon from './person.svg?react';
 import restartIcon from './restart.svg';
 import SirenIcon from './siren.svg?react';
+import CloudIcon from './cloud.svg';
+import SnowIcon from './snow.svg';
+import ThermostatIcon from './thermostat.svg';
+import WarmIcon from './warm.svg';
+import ColorSirenIcon from './color-siren.svg';
 
-export { AlarmIcon, PersonIcon, ArrowLeftIcon, SirenIcon, EnvelopeIcon, BoardIcon, restartIcon };
+export {
+  AlarmIcon,
+  PersonIcon,
+  ArrowLeftIcon,
+  SirenIcon,
+  EnvelopeIcon,
+  BoardIcon,
+  restartIcon,
+  CloudIcon,
+  SnowIcon,
+  ThermostatIcon,
+  WarmIcon,
+  ColorSirenIcon,
+};
