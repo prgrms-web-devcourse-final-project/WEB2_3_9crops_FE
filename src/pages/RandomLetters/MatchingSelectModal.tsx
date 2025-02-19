b64bdc3f18bc6994407896b61f79c1d898ccc3c7
안지원 <wl990@naver.com>
2025-02-20 08:56:40 +0900

    design:랜덤편지 페이지 퍼블리싱

--- src/pages/RandomLetters/MatchingSelectModal.tsx
+++ src/pages/RandomLetters/MatchingSelectModal.tsx
@@ -1,34 +1,43 @@
+import ModalOverlay from '@/components/ModalOverlay';
+import ResultLetter from '@/components/ResultLetter';
+
 function MatchingSelectModal({
   setOpenModal,
+  selectedLetter,
 }: {
   setOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
+  selectedLetter: SelectedLetter;
 }) {
   return (
-    <div className="absolute top-0 left-0 flex h-full w-full flex-col items-center justify-center bg-black/60">
-      <div className="body-sb flex flex-col items-center gap-1 text-white">
-        <span>이 편지에 답장 하시겠어요?</span>
-        <span>수락한 편지는 5분이 지나면 취소할 수 없습니다.</span>
+    <ModalOverlay>
+      <div className="flex-col items-center justify-center">
+        <div className="body-sb flex flex-col items-center gap-1 text-white">
+          <span>이 편지에 답장 하시겠어요?</span>
+          <span>수락한 편지는 5분이 지나면 취소할 수 없습니다.</span>
+        </div>
+        <div className="mt-4 w-full">
+          <ResultLetter stampName={selectedLetter.stampName} title={selectedLetter.title} />
+        </div>
+        <div className="mt-12.5 flex w-[300px] gap-4">
+          <button
+            className="bg-primary-4 body-m h-10 w-[calc(50%-8px)] rounded-lg text-gray-50"
+            onClick={() => {
+              setOpenModal(false);
+            }}
+          >
+            거부하기
+          </button>
+          <button
+            className="bg-primary-3 body-m h-10 w-[calc(50%-8px)] rounded-lg"
+            onClick={() => {
+              setOpenModal(false);
+            }}
+          >
+            승인하기
+          </button>
+        </div>
       </div>
-      <div className="bg-accent-3 mt-4 h-50 w-75"></div>
-      <div className="mt-12.5 flex w-[300px] gap-4">
-        <button
-          className="bg-primary-4 body-m h-10 w-[calc(50%-8px)] rounded-lg text-gray-50"
-          onClick={() => {
-            setOpenModal(false);
-          }}
-        >
-          거부하기
-        </button>
-        <button
-          className="bg-primary-3 body-m h-10 w-[calc(50%-8px)] rounded-lg"
-          onClick={() => {
-            setOpenModal(false);
-          }}
-        >
-          승인하기
-        </button>
-      </div>
-    </div>
+    </ModalOverlay>
   );
 }
 export default MatchingSelectModal;
--- src/pages/RandomLetters/index.tsx
+++ src/pages/RandomLetters/index.tsx
@@ -1,89 +1,41 @@
 import { useState } from 'react';
-import { EffectCards } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { twMerge } from 'tailwind-merge';
-
-import 'swiper/swiper-bundle.css';
-
-import { restartIcon } from '@/assets/icons';
-import BgItem from '@/assets/images/field-4.png';
-import ResultLetter from '@/components/ResultLetter';
-
+import MatchingSelect from './MatchingSelect';
 import MatchingSelectModal from './MatchingSelectModal';
+import Matched from './Matched';
+import BgItem from '@/assets/images/field-4.png';
+import LetterDetail from '@/components/LetterDetail';
 
 const RandomLettersPage = () => {
-  const categoryList = ['전체', '위로와 공감', '축하와 응원', '고민 상담', '기타'];
-  const [selectedCategory, setSelectedCategory] = useState('전체');
   const [openModal, setOpenModal] = useState<boolean>(false);
+  const [selectedLetter, setSelectedLetter] = useState<SelectedLetter>({
+    stampName: '기타',
+    title: 'error',
+  });
 
-  const DUMMY_LIST: { stampName: Stamp; title: string }[] = [
-    { stampName: '위로와 공감', title: '위로가 필요해요' },
-    { stampName: '축하와 응원', title: '저에게 미움받을 용기를 주세요' },
-    { stampName: '고민 상담', title: '삶이 무료해서 고민이에요' },
-    { stampName: '기타', title: '어제 꾼 꿈이 신기했어요' },
-    { stampName: '고민 상담', title: '삶이 유료해서 고민이에요' },
-    { stampName: '축하와 응원', title: '어제 취업했어요!!!!' },
-    { stampName: '축하와 응원', title: '어제 게임 신기록 세웠어요!!!!!' },
-    { stampName: '기타', title: '기타는 핑거스타일이 멋있는거 같아요' },
-    { stampName: '위로와 공감', title: '10년지기 친구가 이사를 가요' },
-    {
-      stampName: '기타',
-      title:
-        '햄부기햄북 햄북어 햄북스딱스 함부르크햄부가우가 햄비기햄부거 햄부가티햄부기온앤 온 을 차려오거라.',
-    },
-  ];
+  const DUMMY = {
+    title: '나에게 햄버거 햄부기우기우가 햄북스따스 함부르크 햄버거링고를 대령하거라 ',
+    text: '이 편지는 영국에서 최초로 시작되어 일년에 한바퀴를 돌면서 받는 사람에게 행운을 주었고 지금은 당신에게로 옮겨진 이 편지는 4일 안에 당신 곁을 떠나야 합니다. 이 편지를 포함해서 7통을 행운이 필요한 사람에게 보내 주셔야 합니다. 복사를 해도 좋습니다. 혹 미신이라 하실지 모르지만 사실입니다.영국에서 HGXWCH이라는 사람은 1930년에 이 편지를 받았습니다. 그는 비서에게 복사해서 보내라고 했습니다. 며칠 뒤에 복권이 당첨되어 20억을 받았습니다. 어떤 이는 이 편지를 받았으나 96시간 이내 자신의 손에서 떠나야 한다는 사실을 잊었습니다. 그는 곧 사직되었습니다. 나중에야 이 사실을 알고 7통의 편지를 보냈는데 다시 좋은 직장을 얻었습니다. 미국의 케네디 대통령은 이 편지를 받았지만 그냥 버렸습니다. 결국 9일 후 그는 암살당했습니다. 기억해 주세요. 이 편지를 보내면 7년의 행운이 있을 것이고 그렇지 않으면 3년의 불행이 있을 것입니다. ',
+  };
 
   return (
     <>
-      {openModal && <MatchingSelectModal setOpenModal={setOpenModal} />}
-      <div className="flex flex-col items-center">
-        <span className="text-gray-60 body-b mt-20 rounded-full bg-white px-6 py-4">
-          답장하고 싶은 편지를 선택해주세요!
-        </span>
-        <div className="mt-25 flex flex-col items-start gap-1.5">
-          <button className="flex items-center gap-1">
-            <img src={restartIcon} alt="재시작 아이콘" />
-            <span className="caption-sb text-gray-30" onClick={() => {}}>
-              리스트 새로고침
-            </span>
-          </button>
-          <div className="mx-auto w-full max-w-75">
-            <Swiper effect={'cards'} grabCursor={true} modules={[EffectCards]} className="mySwiper">
-              {DUMMY_LIST.map((list, idx) => {
-                return (
-                  <SwiperSlide key={idx}>
-                    <div className="w-full cursor-pointer" onClick={() => setOpenModal(true)}>
-                      <ResultLetter stampName={list.stampName} title={list.title} />
-                    </div>
-                  </SwiperSlide>
-                );
-              })}
-            </Swiper>
-          </div>
+      {false && (
+        <div className="flex grow flex-col items-center overflow-hidden">
+          <span className="text-gray-60 body-b mt-20 rounded-full bg-white px-6 py-4">
+            답장하고 싶은 편지를 선택해주세요!
+          </span>
+          <MatchingSelect setOpenModal={setOpenModal} setSelectedLetter={setSelectedLetter} />
+          {/* <Matched /> */}
+          <div
+            className="fixed bottom-[-40px] left-1/2 z-[-10] h-42 w-full -translate-x-1/2 bg-[image:var(--bg-image)] bg-[length:100%_100%] bg-center opacity-70"
+            style={{ '--bg-image': `url(${BgItem})` } as React.CSSProperties}
+          />
+          {openModal && (
+            <MatchingSelectModal setOpenModal={setOpenModal} selectedLetter={selectedLetter} />
+          )}
         </div>
-        <div className="mt-11 flex w-60 flex-wrap items-center justify-center gap-2">
-          {categoryList.map((category, idx) => {
-            return (
-              <button
-                onClick={() => {
-                  setSelectedCategory(category);
-                }}
-                className={twMerge(
-                  `body-b text-gray-60 rounded-full bg-white px-3 py-1.5`,
-                  selectedCategory === category && 'bg-primary-1 text-white',
-                )}
-                key={idx}
-              >
-                {category}
-              </button>
-            );
-          })}
-        </div>
-        <div
-          className="fixed bottom-[-40px] left-1/2 z-[-10] h-42 w-full -translate-x-1/2 bg-[image:var(--bg-image)] bg-[length:100%_100%] bg-center opacity-70"
-          style={{ '--bg-image': `url(${BgItem})` } as React.CSSProperties}
-        />
-      </div>
+      )}
+      <LetterDetail title={DUMMY.title} text={DUMMY.text}></LetterDetail>
     </>
   );
 };
