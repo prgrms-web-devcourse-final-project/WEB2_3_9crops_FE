306ff325f2c856191836e51a7b9454bd28922142
AAminha <dks4857@gmail.com>
2025-02-19 12:05:41 +0900

    design: 모달 활성화 시, 스크롤이 안되도록 수정

--- src/components/ModalOverlay.tsx
+++ src/components/ModalOverlay.tsx
@@ -1,10 +1,28 @@
+import { useEffect } from 'react';
+
+import { getScrollbarWidth } from '@/utils/getScrollbarWidth';
+
 interface ModalOverlayProps {
   closeOnOutsideClick?: boolean;
-  children: React.ReactElement;
+  children: React.ReactNode;
   onClose?: () => void;
 }
 
 const ModalOverlay = ({ closeOnOutsideClick = false, children, onClose }: ModalOverlayProps) => {
+  useEffect(() => {
+    const scrollbarWidth = getScrollbarWidth();
+    const header = document.querySelector('header');
+
+    document.documentElement.style.setProperty('--scrollbar-width', `${scrollbarWidth}px`);
+    document.body.classList.add('modal-open');
+    if (header) header.classList.add('modal-open');
+
+    return () => {
+      document.documentElement.style.setProperty('--scrollbar-width', '0px');
+      document.body.classList.remove('modal-open');
+    };
+  }, []);
+
   const handleClickOutside = () => {
     if (closeOnOutsideClick && onClose) {
       onClose();
