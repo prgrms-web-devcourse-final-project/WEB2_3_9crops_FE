8c3c23c321561841f0678c60f3472d575c353de7
wldnjs990 <139528356+wldnjs990@users.noreply.github.com>
2025-02-19 16:35:35 +0900

    design: 편지작성 페이지 퍼블리싱 (#13)
    * design:편지 작성 페이지 작업 시작
    
    * design:폰트 css 추가
    
    * design:편지 작성 페이지 작업중
    
    * design:글 작성 페이지 관련 이미지
    
    * design:애니메이션 요소, 테마 제외한 퍼블리싱 완료
    
    * design:편지지 테마선택 퍼블리싱 완료
    
    * refactor:타입명 파스칼 케이스로 변경 & 상수명 대문자로 변경 & 타입 export import문 제거 & 몇몇 코드 상수파일 통합 & 타입파일 types폴더로 이동
    
    * refactor:코드리뷰 수정사항 적용
    
    * design:편지작성 완료 애니메이션 구현
    
    * design:고민편지 작성 결과에도 애니메이션 적용 & ResultLetter 컴포넌트 공용 컴포넌트로 이동

---
+++ src/pages/Write/components/PreviousLetterOption.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+export default function PreviousLetterOption({ prevLetter = [] }: { prevLetter: PrevLetter }) {
+  const [index, setIndex] = useState(0);
+  return (
+    <div className="flex w-full flex-col gap-3 px-4 pt-3 pb-[30px]">
+      <div className="flex flex-col gap-1.5">
+        <div className="caption-m flex justify-between">
+          <span>{prevLetter![index].title}</span>
+          <div className="flex gap-1">
+            {prevLetter?.map((_, idx) => {
+              return (
+                <button
+                  className={twMerge(
+                    `flex h-5 w-5 cursor-pointer items-center justify-center rounded-full bg-white`,
+                    idx === index && 'bg-primary-1',
+                  )}
+                  onClick={() => setIndex(idx)}
+                  key={idx}
+                >
+                  {idx + 1}
+                </button>
+              );
+            })}
+          </div>
+        </div>
+
+        <div className="h-[305px] w-full overflow-y-scroll rounded-lg bg-white px-4 py-2">
+          {prevLetter![index].text}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/stores/writeStore.ts
@@ -0,0 +1,33 @@
+import { create } from 'zustand';
+
+interface WriteStore {
+  letterTitle: string;
+  setLetterTitle: (typing: string) => void;
+  letterText: string;
+  setLetterText: (typing: string) => void;
+  font: string;
+  setFont: (selectedFont: string) => void;
+  theme: Theme;
+  setTheme: (selectedTheme: Theme) => void;
+  stamp: Stamp;
+  setStamp: (selectedStamp: Stamp) => void;
+}
+const useWrite = create<WriteStore>((set) => ({
+  letterTitle: '',
+  setLetterTitle: (typing) => set(() => ({ letterTitle: typing })),
+  letterText: '',
+  setLetterText: (typing) => set(() => ({ letterText: typing })),
+  font: 'pretendard',
+  setFont: (selectedFont) => {
+    set(() => ({ font: selectedFont }));
+  },
+  theme: '기본',
+  setTheme: (selectedTheme) =>
+    set(() => ({
+      theme: selectedTheme,
+    })),
+  stamp: '위로와 공감',
+  setStamp: (selectedStamp) => set(() => ({ stamp: selectedStamp })),
+}));
+
+export default useWrite;
