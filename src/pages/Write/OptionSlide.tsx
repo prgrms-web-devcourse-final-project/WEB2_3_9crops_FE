83500ef1ca4a7feb2c99eeefda58c45e88e269aa
wl990@naver.com <wl990@naver.com>
2025-02-20 17:13:28 +0900

    design : 옵션 슬라이드 바텀시트 애니메이션 구현(약간 미완)

--- src/pages/Write/OptionSlide.tsx
+++ src/pages/Write/OptionSlide.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef, useState } from 'react';
-import { twMerge } from 'tailwind-merge';
 
 import FontOption from './components/FontOption';
 import PreviousLetterOption from './components/PreviousLetterOption';
@@ -7,49 +6,58 @@ import ThemeOption from './components/ThemeOption';
 import WritePageButton from './components/WritePageButton';
 
 function OptionSlide({ prevLetter }: { prevLetter: PrevLetter }) {
-  const [clicked, setClicked] = useState<Option>(null);
+  const [target, setTarget] = useState<Option>('편지지');
   const [slideActive, setSlideActive] = useState<boolean>(false);
   const buttonTitle = ['편지지', '글꼴', '이전 편지 내용'];
 
-  const headRef = useRef<HTMLDivElement>(null);
+  const slideRef = useRef<HTMLDivElement>(null);
   const optionRef = useRef<HTMLDivElement>(null);
-  const [computedHeight, setComputedHeight] = useState<number>(0);
 
   useEffect(() => {
-    const head = headRef.current?.offsetHeight;
-    const option = optionRef.current?.offsetHeight;
-    console.log(head, option);
-    if (head && option) {
-      setComputedHeight(head - option);
-      console.log(head - option);
-    }
-  }, []);
-
-  // 슬라이드 애니메이션을 줘야 하는데 마음데로 안되네에에엑
-  const slideStyle = twMerge(
-    `bg-primary-3 fixed bottom-0 left-[50%] flex w-full max-w-[600px] translate-x-[-50%] flex-col rounded-t-[20px] duration-100`,
-    slideActive && 'translate-y-0',
-  );
+    const handleSlideButton = () => {
+      // ref가 처음 높이를 못 받아오는거 같아서 비동기로 후처리함
+      setTimeout(() => {
+        if (slideRef.current) {
+          if (slideActive) {
+            slideRef.current.style.transform = `translateY(${0}px)`;
+          } else {
+            slideRef.current.style.transform = `translateY(${optionRef.current?.offsetHeight}px)`;
+          }
+        }
+      }, 0);
+      if (!slideRef.current?.style.transition) {
+        setTimeout(() => {
+          if (slideRef.current) slideRef.current.style.transition = 'all .3s';
+        }, 100);
+      }
+    };
+    handleSlideButton();
+  }, [slideActive]);
 
   return (
-    <div className={slideStyle}>
-      <div
-        ref={headRef}
-        className="border-primary-1 flex cursor-pointer items-center justify-between border-b-2 px-4 pt-6 pb-4"
-      >
+    <div
+      ref={slideRef}
+      className="bg-primary-3 fixed bottom-0 left-[50%] flex w-full max-w-[600px] translate-x-[-50%] flex-col rounded-t-[20px]"
+    >
+      <div className="border-primary-1 flex cursor-pointer items-center justify-between border-b-2 px-4 pt-6 pb-4">
         <div className="flex gap-2">
           <WritePageButton
             text={buttonTitle[0]}
             bgColor="white"
             rounded="lg"
-            clicked={clicked}
+            target={target}
+            slideActive={slideActive}
             onClick={() => {
-              if (clicked !== buttonTitle[0]) {
-                setClicked('편지지');
+              if (target !== buttonTitle[0]) {
+                setTarget('편지지');
                 setSlideActive(() => true);
-              } else {
-                setClicked(null);
-                setSlideActive(() => false);
+              }
+              if (target === buttonTitle[0]) {
+                if (slideActive) {
+                  setSlideActive(false);
+                } else {
+                  setSlideActive(true);
+                }
               }
             }}
           />
@@ -57,14 +65,19 @@ function OptionSlide({ prevLetter }: { prevLetter: PrevLetter }) {
             text={buttonTitle[1]}
             bgColor="white"
             rounded="lg"
-            clicked={clicked}
+            target={target}
+            slideActive={slideActive}
             onClick={() => {
-              if (clicked !== buttonTitle[1]) {
-                setClicked('글꼴');
+              if (target !== buttonTitle[1]) {
+                setTarget('글꼴');
                 setSlideActive(() => true);
-              } else {
-                setClicked(null);
-                setSlideActive(() => false);
+              }
+              if (target === buttonTitle[1]) {
+                if (slideActive) {
+                  setSlideActive(false);
+                } else {
+                  setSlideActive(true);
+                }
               }
             }}
           />
@@ -74,23 +87,29 @@ function OptionSlide({ prevLetter }: { prevLetter: PrevLetter }) {
             text={buttonTitle[2]}
             bgColor="white"
             rounded="lg"
-            clicked={clicked}
+            target={target}
+            slideActive={slideActive}
             onClick={() => {
-              if (clicked !== buttonTitle[2]) {
-                setClicked('이전 편지 내용');
+              if (target !== buttonTitle[2]) {
+                setTarget('이전 편지 내용');
                 setSlideActive(() => true);
-              } else {
-                setClicked(null);
-                setSlideActive(() => false);
+              }
+              if (target === buttonTitle[2]) {
+                if (slideActive) {
+                  setSlideActive(false);
+                } else {
+                  setSlideActive(true);
+                }
               }
             }}
           />
         )}
       </div>
       <div ref={optionRef}>
-        {clicked === '편지지' && <ThemeOption />}
-        {clicked === '글꼴' && <FontOption />}
-        {clicked === '이전 편지 내용' && <PreviousLetterOption prevLetter={prevLetter} />}
+        {!target && <div className="h-50 w-full"></div>}
+        {target === '편지지' && <ThemeOption />}
+        {target === '글꼴' && <FontOption />}
+        {target === '이전 편지 내용' && <PreviousLetterOption prevLetter={prevLetter} />}
       </div>
     </div>
   );
