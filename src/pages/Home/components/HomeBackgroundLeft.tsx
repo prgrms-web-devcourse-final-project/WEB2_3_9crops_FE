6755831ebd583a307d2b55c17b1ca34910e16ccb
Seungyeon Han <tiffanyhansy@gmail.com>
2025-02-21 06:49:08 +0900

    design: 홈 페이지 왼쪽 배경 width 조정

--- src/pages/Home/components/HomeBackgroundLeft.tsx
+++ src/pages/Home/components/HomeBackgroundLeft.tsx
@@ -5,7 +5,7 @@ const HomeBackgroundLeft = () => {
   return (
     <BackgroundImageWrapper
       as="div"
-      className="absolute bottom-0 left-0 z-[11] h-[calc(var(--vh)*25)] w-full min-w-[800px] -translate-x-1/3"
+      className="absolute bottom-0 left-0 z-[11] h-[calc(var(--vh)*25)] w-full min-w-[700px] -translate-x-1/3"
       imageUrl={homeLeftMountain}
     />
   );
