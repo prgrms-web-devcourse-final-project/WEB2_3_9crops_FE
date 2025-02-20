d2238430d61a7b1d28f3fe2448b40c808a96084f
Seungyeon Han <tiffanyhansy@gmail.com>
2025-02-21 07:10:40 +0900

    design: 게시판 이미지 버튼 width 조정

--- src/pages/Home/components/GoToLetterBoard.tsx
+++ src/pages/Home/components/GoToLetterBoard.tsx
@@ -3,7 +3,7 @@ import goToLetterBoard from '@/assets/images/go-to-letter-board.png';
 
 const GoToLetterBoard = () => {
   return (
-    <div className="absolute bottom-48 left-[calc(var(--vh)*36)] z-9 flex w-fit">
+    <div className="absolute bottom-48 left-[calc(var(--vh)*36)] z-9 flex w-full">
       <div className="text-left">
         <p className="text-gray-60 body-r mb-1 ml-2">게시판</p>
         <Link to="/letter/board">
