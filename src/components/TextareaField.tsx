f513809cc17683f92ebd65889a7140f19bc2789c
AAminha <dks4857@gmail.com>
2025-02-21 22:08:14 +0900

    refactor: 중복되는 모달 배경 컴포넌트 적용 및 불필요한 에셋 제거

--- src/components/TextareaField.tsx
+++ src/components/TextareaField.tsx
@@ -2,7 +2,7 @@ import { ComponentPropsWithoutRef } from 'react';
 
 const TextareaField = ({ ...props }: ComponentPropsWithoutRef<'textarea'>) => {
   return (
-    <div className="w-full rounded-sm bg-white px-3 py-1.5">
+    <div className="relative z-1 w-full rounded-sm bg-white px-3 py-1.5">
       <textarea
         className="body-m placeholder:text-gray-30 text-gray-80 w-full resize-none bg-transparent"
         {...props}
