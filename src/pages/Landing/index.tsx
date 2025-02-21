4c90dbc5c5ade02ea60437b095112c993a7b602a
Minha Ahn <dks4857@gmail.com>
2025-02-21 11:05:19 +0900

    design: 로그인 및 랜딩 페이지 퍼블리싱 (#22)
    * design: 전체 배경 그라데이션 삽입
    
    * design: 로그인 페이지 에셋 추가
    
    * design: 로그인 페이지 퍼블리싱
    
    * design: 로그인 페이지 에셋 변경 및 반영
    
    * design: 랜딩페이지 퍼블리싱
    
    * remove: 불필요한 에셋 제거
    
    * remove: 중복 에셋 제거
    
    * refactor: 로그인 아이콘에 aria-label 추가
    
    * style: 랜딩페이지 원 크기 수정

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import { Navigate } from 'react-router';
+import { twMerge } from 'tailwind-merge';
+
+import LandingImg from '@/assets/images/landing.png';
+
+import { STYLE_CLASS } from './constants';
+
+const Landing = () => {
+  const [step, setStep] = useState(0);
+
+  if (step === 3) return <Navigate to="/login" />;
+
+  return (
+    <main className="relative flex grow justify-center" onClick={() => setStep((prev) => prev + 1)}>
+      <img
+        src={LandingImg}
+        alt="서비스 소개 이미지"
+        className={twMerge(
+          'fixed bottom-0 h-70 w-auto max-w-none -translate-x-1/2 transition-all duration-200',
+          STYLE_CLASS[step].imagePosition,
+        )}
+      />
+      <section
+        className={twMerge(
+          'fixed z-1 -translate-x-1/2 transition-all duration-200',
+          STYLE_CLASS[step].mask,
+        )}
+      >
+        <p
+          className={twMerge(
+            'body-sb absolute z-2 w-max -translate-x-1/2 whitespace-pre-wrap text-white transition-all duration-200',
+            STYLE_CLASS[step].textPosition,
+          )}
+        >
+          {STYLE_CLASS[step].description}
+        </p>
+        <div
+          className={twMerge(
+            'rounded-full shadow-[0_0_0_9999px_rgba(0,0,0,0.5)]',
+            STYLE_CLASS[step].circle,
+          )}
+        />
+      </section>
+    </main>
+  );
+};
+
+export default Landing;
