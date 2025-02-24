ff7fe17c1a63331c9cacfc2220eff44b3a4b35bb
wl990@naver.com <wl990@naver.com>
2025-02-24 22:46:09 +0900

    feat:차단된 편지 버튼 disabled처리

--- src/pages/LetterDetail/index.tsx
+++ src/pages/LetterDetail/index.tsx
@@ -43,7 +43,6 @@ const LetterDetailPage = () => {
     document.body.addEventListener('click', handleOutsideClick);
     if (params.id) {
       getLetter(params.id, setLetterDetail);
-      // 편지 삭제 요청 테스트(내일 삭제 버튼 만들어서 여기다 추가하긔)
     }
 
     return () => {
@@ -120,12 +119,13 @@ const LetterDetailPage = () => {
         ></textarea>
         <span className="body-sb mt-10 flex justify-end">FROM. {'12E12'}</span>
         <button
-          className="bg-primary-3 body-m mt-3 w-full rounded-lg py-2"
+          className="bg-primary-3 disabled:bg-gray-30 body-m mt-3 w-full rounded-lg py-2 disabled:text-white"
           onClick={() => {
             navigate(`/letter/write/?letterId=${letterDetail?.letterId}`);
           }}
+          disabled={!letterDetail?.matched}
         >
-          편지 작성하기
+          {letterDetail?.matched ? '편지 작성하기' : '대화가 종료된 편지입니다.'}
         </button>
         {deleteModalOpen && (
           <ConfirmModal
