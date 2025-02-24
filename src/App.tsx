70ae3a3f8bb09184de0540b4a6bbbc201fe6b84f
Sebin Kim <108220388+nirii00@users.noreply.github.com>
2025-02-24 21:48:23 +0900

    design: 404 페이지 디자인 (#33)
    - 404 페이지 퍼블리싱
    - 홈으로 가는 버튼 추가
    
    Co-authored-by: nirii00 <nirii00@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import LetterBoxDetailPage from './pages/LetterBoxDetail';
 import LetterDetailPage from './pages/LetterDetail';
 import LoginPage from './pages/Login';
 import MyPage from './pages/MyPage';
+import NotFoundPage from './pages/NotFound';
 import NotificationsPage from './pages/Notifications';
 import OnboardingPage from './pages/Onboarding';
 import RandomLettersPage from './pages/RandomLetters';
@@ -51,7 +52,9 @@ const App = () => {
           <Route path="board" element={<LetterBoardPage />} />
           <Route path="notifications" element={<NotificationsPage />} />
         </Route>
+        <Route path="*" element={<NotFoundPage />}></Route>
       </Route>
+
       <Route path="admin" element={<AdminPage />}>
         <Route path="report" element={<ReportManage />} />
       </Route>
