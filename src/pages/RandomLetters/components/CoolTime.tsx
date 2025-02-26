5dc58dcf9a7c0a08ff67ae345a687c1711bc98c1
안지원 <wl990@naver.com>
2025-02-26 22:04:22 +0900

    feat:랜덤편지 페이지 매칭 제한시간, 쿨타임 로직 구현

--- src/pages/RandomLetters/components/CoolTime.tsx
+++ src/pages/RandomLetters/components/CoolTime.tsx
@@ -1,22 +1,77 @@
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 
 import LetterWrapper from '@/components/LetterWrapper';
 
 // import letterPink from '@/assets/images/letter-pink.png';
 
-export default function CoolTime() {
+export default function CoolTime({
+  setCoolTime,
+}: {
+  setCoolTime: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
   const navigate = useNavigate();
 
-  const DUMMY_TIME = new Date();
-  const hours = DUMMY_TIME.getHours();
-  const minuites = DUMMY_TIME.getMinutes();
-  const seconds = DUMMY_TIME.getSeconds();
+  const TIME_STAMP = '2025-02-26T22:13:25.262045608';
+
+  const COMPLETED_DATE = new Date(TIME_STAMP);
+
+  const END_DATE = new Date(COMPLETED_DATE);
+  END_DATE.setHours(COMPLETED_DATE.getHours() + 1);
+
+  const NOW_DATE = new Date();
+
+  const endTime = END_DATE.getTime() - NOW_DATE.getTime();
+
+  const [endTimes, setEndTimes] = useState({
+    hours: Math.floor((endTime / (1000 * 60 * 60)) % 24),
+    minutes: Math.floor((endTime / (1000 * 60)) % 60),
+    seconds: Math.floor((endTime / 1000) % 60),
+  });
+
+  useEffect(() => {
+    if (endTimes.hours < 0 || endTimes.minutes < 0 || endTimes.seconds < 0) {
+      setEndTimes({ hours: 0, minutes: 0, seconds: 0 });
+    }
+    if (endTimes.hours === 0 && endTimes.minutes === 0 && endTimes.seconds === 0) {
+      setCoolTime(false);
+      return;
+    }
+    const endTimeFlow = setInterval(() => {
+      setEndTimes((currentTime) => {
+        if (currentTime.seconds > 0) {
+          return { ...currentTime, seconds: currentTime.seconds - 1 };
+        } //
+        else {
+          if (currentTime.minutes > 0) {
+            return { ...currentTime, minutes: currentTime.minutes - 1, seconds: 59 };
+          } //
+          else {
+            if (currentTime.hours > 0) {
+              return { hours: currentTime.hours - 1, minutes: 59, seconds: 59 };
+            } //
+            else {
+              setCoolTime(false);
+              return { ...currentTime };
+            }
+          }
+        }
+      });
+      if (endTimes.hours === 0 && endTimes.minutes === 0 && endTimes.seconds === 0) {
+        clearInterval(endTimeFlow);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(endTimeFlow);
+    };
+  }, [endTimes, setCoolTime]);
   return (
     <div className="mt-20 flex flex-col items-center justify-center">
       <div className="body-m flex flex-col items-center justify-center">
         <p className="text-gray-60">편지 매칭 활성화 까지</p>
         <p className="text-gray-80">
-          {hours} : {minuites} : {seconds}
+          {endTimes.hours} : {endTimes.minutes} : {endTimes.seconds}
         </p>
         <div className="mt-2 w-75">
           <LetterWrapper>
--- src/pages/RandomLetters/components/Matched.tsx
+++ src/pages/RandomLetters/components/Matched.tsx
@@ -2,68 +2,76 @@ import { useEffect, useState } from 'react';
 
 import ResultLetter from '@/components/ResultLetter';
 
-export default function Matched() {
-  const TIME_STAMP = '2025-02-26T18:11:46.262045608';
+export default function Matched({
+  setMatched,
+  setCoolTime,
+}: {
+  setMatched: React.Dispatch<React.SetStateAction<boolean>>;
+  setCoolTime: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  const [isDisabled, setIsDisabled] = useState<boolean>(false);
 
-  const DUMMY_DATE = new Date(TIME_STAMP);
+  const TIME_STAMP = '2025-02-25T21:52:25.262045608';
 
-  const END_DATE = new Date(DUMMY_DATE);
-  END_DATE.setHours(DUMMY_DATE.getHours() + 24);
+  const MATCHED_DATE = new Date(TIME_STAMP);
 
-  const GRACE_DATE = new Date(DUMMY_DATE);
-  GRACE_DATE.setMinutes(DUMMY_DATE.getMinutes() + 5);
+  const END_DATE = new Date(MATCHED_DATE);
+  END_DATE.setHours(MATCHED_DATE.getHours() + 24);
+
+  const GRACE_DATE = new Date(MATCHED_DATE);
+  GRACE_DATE.setMinutes(MATCHED_DATE.getMinutes() + 5);
 
   const NOW_DATE = new Date();
 
   const endTime = END_DATE.getTime() - NOW_DATE.getTime();
   const graceTime = GRACE_DATE.getTime() - NOW_DATE.getTime();
 
-  const [endHours, setEndHours] = useState(Math.floor((endTime / (1000 * 60 * 60)) % 24));
-  const [endMinutes, setEndMinutes] = useState(Math.floor((endTime / (1000 * 60)) % 60));
-  const [endSeconds, setEndSeconds] = useState(Math.floor((endTime / 1000) % 60));
+  const [endTimes, setEndTimes] = useState({
+    hours: Math.floor((endTime / (1000 * 60 * 60)) % 24),
+    minutes: Math.floor((endTime / (1000 * 60)) % 60),
+    seconds: Math.floor((endTime / 1000) % 60),
+  });
 
-  const [graceMinutes, setGraceMinutes] = useState(Math.floor((graceTime / (1000 * 60)) % 60));
-  const [graceSeconds, setGraceSeconds] = useState(Math.floor((graceTime / 1000) % 60));
+  const [graceTimes, setGraceTimes] = useState({
+    minutes: Math.floor((graceTime / (1000 * 60)) % 60),
+    seconds: Math.floor((graceTime / 1000) % 60),
+  });
+
+  const formatNumber = (num: number) => {
+    return num.toString().padStart(2, '0');
+  };
 
   useEffect(() => {
-    if (endHours < 0 || endMinutes < 0 || endSeconds < 0) {
-      setEndHours(0);
-      setEndMinutes(0);
-      setEndSeconds(0);
+    if (endTimes.hours < 0 || endTimes.minutes < 0 || endTimes.seconds < 0) {
+      setEndTimes({ hours: 0, minutes: 0, seconds: 0 });
     }
-
+    if (endTimes.hours === 0 && endTimes.minutes === 0 && endTimes.seconds === 0) {
+      setMatched(false);
+      setCoolTime(true);
+      return;
+    }
     const endTimeFlow = setInterval(() => {
-      setEndSeconds((prevSeconds) => {
-        if (prevSeconds > 0) {
-          return prevSeconds - 1;
-        } else {
-          setEndMinutes((prevMinutes) => {
-            if (prevMinutes > 0) {
-              return prevMinutes - 1;
-            } else {
-              setEndHours((prevendHours) => {
-                if (prevendHours > 0) {
-                  return prevendHours - 1;
-                } else {
-                  return 0;
-                }
-              });
-              if (endHours > 0) {
-                return 59;
-              } else {
-                return 0;
-              }
+      setEndTimes((currentTime) => {
+        if (currentTime.seconds > 0) {
+          return { ...currentTime, seconds: currentTime.seconds - 1 };
+        } //
+        else {
+          if (currentTime.minutes > 0) {
+            return { ...currentTime, minutes: currentTime.minutes - 1, seconds: 59 };
+          } //
+          else {
+            if (currentTime.hours > 0) {
+              return { hours: currentTime.hours - 1, minutes: 59, seconds: 59 };
+            } //
+            else {
+              setMatched(false);
+              setCoolTime(true);
+              return { ...currentTime };
             }
-          });
-          if (endMinutes > 0) {
-            return 59;
-          } else {
-            return 0;
           }
         }
       });
-      if (endHours === 0 && endMinutes === 0 && endSeconds === 0) {
-        console.log('몬가몬가 로직 발동!');
+      if (endTimes.hours === 0 && endTimes.minutes === 0 && endTimes.seconds === 0) {
         clearInterval(endTimeFlow);
       }
     }, 1000);
@@ -71,36 +79,31 @@ export default function Matched() {
     return () => {
       clearInterval(endTimeFlow);
     };
-  }, [endHours, endMinutes, endSeconds]);
+  }, [endTimes, setMatched, setCoolTime]);
 
   useEffect(() => {
-    if (graceMinutes < 0 || graceSeconds < 0) {
-      setGraceMinutes(0);
-      setGraceSeconds(0);
+    if (graceTimes.minutes < 0 || graceTimes.seconds < 0) {
+      setGraceTimes({ minutes: 0, seconds: 0 });
     }
-
+    if (graceTimes.minutes === 0 && graceTimes.seconds === 0) {
+      return setIsDisabled(true);
+    }
     const graceTimeFlow = setInterval(() => {
-      setGraceSeconds((prevSeconds) => {
-        if (prevSeconds > 0) {
-          return prevSeconds - 1;
-        } else {
-          setGraceMinutes((prevMinuites) => {
-            if (prevMinuites > 0) {
-              return prevMinuites - 1;
-            } else {
-              return 0;
-            }
-          });
-          if (graceMinutes > 0) {
-            return 59;
-          } else {
-            return 0;
+      setGraceTimes((currentTime) => {
+        if (currentTime.seconds > 0) {
+          return { ...currentTime, seconds: currentTime.seconds - 1 };
+        } //
+        else {
+          if (currentTime.minutes > 0) {
+            return { minutes: currentTime.minutes - 1, seconds: 59 };
+          } //
+          else {
+            setIsDisabled(true);
+            return { ...currentTime };
           }
         }
       });
-
-      if (graceMinutes === 0 && graceSeconds === 0) {
-        console.log('몬가몬가 로직 발동!');
+      if (graceTimes.minutes === 0 && graceTimes.seconds === 0) {
         clearInterval(graceTimeFlow);
       }
     }, 1000);
@@ -108,20 +111,29 @@ export default function Matched() {
     return () => {
       clearInterval(graceTimeFlow);
     };
-  }, [graceMinutes, graceSeconds]);
+  }, [graceTimes]);
 
   return (
     <div className="mt-20 flex flex-col items-center justify-center">
       <div className="body-m flex flex-col items-center justify-center">
         <p className="text-gray-60">답장까지 남은 시간</p>
         <p className="text-gray-80">
-          {endHours} : {endMinutes} : {endSeconds}
+          {formatNumber(endTimes.hours)} : {formatNumber(endTimes.minutes)} :{' '}
+          {formatNumber(endTimes.seconds)}
         </p>
         <div className="mt-2 w-75">
           <ResultLetter categoryName="ETC" title="테스트용가리" />
         </div>
-        <button className="bg-primary-3 body-m mt-12.5 w-full rounded-lg py-2" onClick={() => {}}>
-          매칭 취소하기 {graceMinutes} : {graceSeconds}
+        <button
+          className="bg-primary-3 body-m disabled:bg-gray-30 mt-12.5 w-full rounded-lg py-2"
+          onClick={() => {
+            console.log('취소');
+          }}
+          disabled={isDisabled}
+        >
+          {isDisabled
+            ? '취소 시간이 지났습니다.'
+            : `답장 취소하기 ${formatNumber(graceTimes.minutes)} : ${formatNumber(graceTimes.seconds)}`}
         </button>
       </div>
     </div>
