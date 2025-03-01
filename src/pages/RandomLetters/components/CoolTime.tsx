45c33313831fdef13c6f128759ff91ea7f4f451a
안지원 <wl990@naver.com>
2025-03-02 00:26:24 +0900

    refactor:매칭 제한시간, 쿨타임 타이머 리팩토링

--- src/pages/RandomLetters/components/CoolTime.tsx
+++ src/pages/RandomLetters/components/CoolTime.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router';
 
 import LetterWrapper from '@/components/LetterWrapper';
 import { formatNumber } from '@/utils/formatNumber';
+import { timeFormatter } from '@/utils/timeFormatter';
 
 // import letterPink from '@/assets/images/letter-pink.png';
 
@@ -13,7 +14,7 @@ export default function CoolTime({
 }) {
   const navigate = useNavigate();
 
-  const TIME_STAMP = '2025-02-26T22:13:25.262045608';
+  const TIME_STAMP = '2025-03-01T21:15:25.262045608';
 
   const COMPLETED_DATE = new Date(TIME_STAMP);
 
@@ -24,56 +25,79 @@ export default function CoolTime({
 
   const endTime = END_DATE.getTime() - NOW_DATE.getTime();
 
-  const [endTimes, setEndTimes] = useState({
-    hours: Math.floor((endTime / (1000 * 60 * 60)) % 24),
-    minutes: Math.floor((endTime / (1000 * 60)) % 60),
-    seconds: Math.floor((endTime / 1000) % 60),
-  });
+  const [endTimeSeconds, setEndTimeSeconds] = useState(endTime / 1000);
+
+  const formatedEndTime = timeFormatter(endTimeSeconds);
 
   useEffect(() => {
-    if (endTimes.hours < 0 || endTimes.minutes < 0 || endTimes.seconds < 0) {
-      setEndTimes({ hours: 0, minutes: 0, seconds: 0 });
-    }
-    if (endTimes.hours === 0 && endTimes.minutes === 0 && endTimes.seconds === 0) {
-      setCoolTime(false);
-      return;
-    }
-    const endTimeFlow = setInterval(() => {
-      setEndTimes((currentTime) => {
-        if (currentTime.seconds > 0) {
-          return { ...currentTime, seconds: currentTime.seconds - 1 };
-        } //
-        else {
-          if (currentTime.minutes > 0) {
-            return { ...currentTime, minutes: currentTime.minutes - 1, seconds: 59 };
-          } //
-          else {
-            if (currentTime.hours > 0) {
-              return { hours: currentTime.hours - 1, minutes: 59, seconds: 59 };
-            } //
-            else {
-              setCoolTime(false);
-              return { ...currentTime };
-            }
-          }
-        }
-      });
-      if (endTimes.hours === 0 && endTimes.minutes === 0 && endTimes.seconds === 0) {
-        clearInterval(endTimeFlow);
+    const endTargetTime = Date.now() + endTime;
+
+    const count = setInterval(() => {
+      const now = Date.now();
+      const newEndTimeSeconds = Math.max(0, Math.floor((endTargetTime - now) / 1000));
+
+      if (endTimeSeconds <= 0) {
+        setCoolTime(false);
       }
+
+      setEndTimeSeconds(newEndTimeSeconds);
     }, 1000);
 
     return () => {
-      clearInterval(endTimeFlow);
+      clearInterval(count);
     };
-  }, [endTimes, setCoolTime]);
+  });
+
+  // const [endTimes, setEndTimes] = useState({
+  //   hours: Math.floor((endTime / (1000 * 60 * 60)) % 24),
+  //   minutes: Math.floor((endTime / (1000 * 60)) % 60),
+  //   seconds: Math.floor((endTime / 1000) % 60),
+  // });
+
+  // useEffect(() => {
+  //   if (endTimes.hours < 0 || endTimes.minutes < 0 || endTimes.seconds < 0) {
+  //     setEndTimes({ hours: 0, minutes: 0, seconds: 0 });
+  //   }
+  //   if (endTimes.hours === 0 && endTimes.minutes === 0 && endTimes.seconds === 0) {
+  //     setCoolTime(false);
+  //     return;
+  //   }
+  //   const endTimeFlow = setInterval(() => {
+  //     setEndTimes((currentTime) => {
+  //       if (currentTime.seconds > 0) {
+  //         return { ...currentTime, seconds: currentTime.seconds - 1 };
+  //       } //
+  //       else {
+  //         if (currentTime.minutes > 0) {
+  //           return { ...currentTime, minutes: currentTime.minutes - 1, seconds: 59 };
+  //         } //
+  //         else {
+  //           if (currentTime.hours > 0) {
+  //             return { hours: currentTime.hours - 1, minutes: 59, seconds: 59 };
+  //           } //
+  //           else {
+  //             setCoolTime(false);
+  //             return { ...currentTime };
+  //           }
+  //         }
+  //       }
+  //     });
+  //     if (endTimes.hours === 0 && endTimes.minutes === 0 && endTimes.seconds === 0) {
+  //       clearInterval(endTimeFlow);
+  //     }
+  //   }, 1000);
+
+  //   return () => {
+  //     clearInterval(endTimeFlow);
+  //   };
+  // }, [endTimes, setCoolTime]);
   return (
     <div className="mt-20 flex flex-col items-center justify-center">
       <div className="body-m flex flex-col items-center justify-center">
         <p className="text-gray-60">랜덤 편지 활성화 까지</p>
         <p className="text-gray-80">
-          {formatNumber(endTimes.hours)} : {formatNumber(endTimes.minutes)} :{' '}
-          {formatNumber(endTimes.seconds)}
+          {formatNumber(formatedEndTime.hours)} : {formatNumber(formatedEndTime.minutes)} :{' '}
+          {formatNumber(formatedEndTime.seconds)}
         </p>
         <div className="mt-2 w-75">
           <LetterWrapper>
