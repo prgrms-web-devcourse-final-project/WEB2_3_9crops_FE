643474cc9a75a999b9a40e131d2d05482784249f
Seungyeon Han <tiffanyhansy@gmail.com>
2025-03-02 17:49:31 +0900

    refactor: hasNewLetters 로컬 상태 제거 및 arrivedCount 전역 상태로 대체

--- src/pages/Home/components/GoToLetterBox.tsx
+++ src/pages/Home/components/GoToLetterBox.tsx
@@ -2,17 +2,18 @@ import { Link } from 'react-router';
 
 import goToLetterBoxNewLetters from '@/assets/images/go-to-letter-box-new-letters.png';
 import goToLetterBox from '@/assets/images/go-to-letter-box.png';
+import { useIncomingLettersStore } from '@/stores/incomingLettersStore';
 
 const GoToLetterBox = () => {
-  //TODO : hasNewLetters 전역으로 상태 관리하기
-  const hasNewLetters = true;
+  const { arrivedCount } = useIncomingLettersStore();
+
   return (
     <div className="absolute bottom-10 left-5 z-9 flex w-fit">
       <div className="text-left">
         <p className="text-gray-60 body-r mb-1 ml-2">내 편지함</p>
         <Link to="/letter/box">
           <img
-            src={hasNewLetters ? goToLetterBoxNewLetters : goToLetterBox}
+            src={arrivedCount ? goToLetterBoxNewLetters : goToLetterBox}
             alt="go to letter box"
             className="w-[206.5px]"
           />
