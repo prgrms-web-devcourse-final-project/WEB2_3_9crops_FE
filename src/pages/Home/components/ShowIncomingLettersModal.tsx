01e9bbbd14fa38e171756f511fff0e58e51b8643
Seungyeon Han (Tiffany) <125551867+tiffanyhansy@users.noreply.github.com>
2025-03-04 21:33:15 +0900

    refactor: PR 리뷰를 반영한 리팩토링 (#70)
    * refactor: 읽지 않은 편지 수 조회 기능 리팩토링
    
    * rename: NewLetterModal 을 UnreadLetterModal로 파일명 및 함수명 변경
    
    * fix: 읽지 않은 편지가 존재할 때 다른 우체통 이미지 보여주도록 수정
    
    * fix: 오고 있는 편지 모달에서 편지 항목 눌렀을 때 보드로 이동하는 에러 해결

--- src/pages/Home/components/ShowIncomingLettersModal.tsx
+++ src/pages/Home/components/ShowIncomingLettersModal.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router';
 
 import ModalBackgroundWrapper from '@/components/ModalBackgroundWrapper';
 import ModalOverlay from '@/components/ModalOverlay';
@@ -11,19 +10,11 @@ interface ShowIncomingLettersModalProps {
 }
 
 const ShowIncomingLettersModal = ({ onClose }: ShowIncomingLettersModalProps) => {
-  const navigate = useNavigate();
-
-  const handleNavigation = (incomingId: number) => {
-    navigate(`/board/letter/${incomingId}`, {
-      state: { isShareLetterPreview: false },
-    });
-  };
-
   const { data, fetchIncomingLetters } = useIncomingLettersStore();
 
   useEffect(() => {
     fetchIncomingLetters();
-  }, []);
+  }, [fetchIncomingLetters]);
 
   return (
     <ModalOverlay closeOnOutsideClick onClose={onClose}>
@@ -42,7 +33,6 @@ const ShowIncomingLettersModal = ({ onClose }: ShowIncomingLettersModalProps) =>
                 <div
                   className="text-gray-80 body-m flex h-10 w-full items-center justify-between gap-1 rounded-lg bg-white p-3"
                   key={letter.letterId}
-                  onClick={() => handleNavigation(letter.letterId)}
                 >
                   <p className="truncate">{letter.title}</p>
                   <p>{letter.remainingTime}</p>
