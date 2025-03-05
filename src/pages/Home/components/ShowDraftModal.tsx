9812d330fccbc60c5604f8d85db5f581887cb86a
Seungyeon Han (Tiffany) <125551867+tiffanyhansy@users.noreply.github.com>
2025-03-05 16:21:33 +0900

    feat: 임시저장된 편지 조회 기능 구현  (#75)
    * refactor: 함수명 통일성을 위해 fetchIncomingLettersApi → getIncomingLetters 변경
    
    * design: 홈 페이지 임시저장 편지 모달에 스크롤 기능 추가
    
    * feat: 임시저장된 편지 조회 기능 구현
    
    * fix: 모달 재오픈 시 임시저장 편지 최신화 되지 않는 문제 수정
    
    * feat: ShowDraftModal에서 임시저장 편지 클릭 시 이동 로직 추가 (주석 처리)

--- src/pages/Home/components/ShowDraftModal.tsx
+++ src/pages/Home/components/ShowDraftModal.tsx
@@ -1,6 +1,8 @@
 import DeleteOutlineRoundedIcon from '@mui/icons-material/DeleteOutlineRounded';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+// import { useNavigate } from 'react-router';
 
+import { DraftLetter, getDraftLetters } from '@/apis/draftLetters';
 import ModalBackgroundWrapper from '@/components/ModalBackgroundWrapper';
 import ModalOverlay from '@/components/ModalOverlay';
 
@@ -9,14 +11,27 @@ interface ShowDraftModalProps {
   onClose: () => void;
 }
 
-const DUMMY_DRAFT = [
-  { id: 1, title: '취업 때문에 고민이 많아요!!' },
-  { id: 2, title: '배고파서 죽을 거 같아요 😭' },
-  { id: 3, title: '개발하니까 밖에 나갈 일이 없어서 너무 심심하고 피곤해요' },
-  { id: 4, title: '마라샹궈 먹고 싶어요' },
-];
-
 const ShowDraftModal = ({ onClose }: ShowDraftModalProps) => {
+  const [draftLetters, setDraftLetters] = useState<DraftLetter[]>([]);
+
+  // const navigate = useNavigate();
+
+  // const handleNavigation = (incomingId: number) => {
+  //   navigate(`/board/letter/${incomingId}`, {
+  //     state: { isShareLetterPreview: false },
+  //   });
+  // };
+
+  useEffect(() => {
+    getDraftLetters()
+      .then((data) => {
+        setDraftLetters(data || []);
+      })
+      .catch((error) => {
+        console.error('❌ 임시저장된 편지를 불러오는데 실패했습니다', error);
+      });
+  }, [onClose]);
+
   return (
     <ModalOverlay closeOnOutsideClick onClose={onClose}>
       <div className="flex h-full flex-col items-center justify-center">
@@ -29,11 +44,12 @@ const ShowDraftModal = ({ onClose }: ShowDraftModalProps) => {
               <p className="body-sb text-gray-80">임시저장 편지</p>
               <p className="caption-r text-black">로그아웃 시 임시 저장된 편지는 사라집니다</p>
             </div>
-            <div className="mt-6 flex w-[251px] flex-col gap-[10px]">
-              {DUMMY_DRAFT.map((draft) => (
+            <div className="mt-6 flex max-h-60 min-h-auto w-[251px] flex-col gap-[10px] overflow-y-scroll [&::-webkit-scrollbar]:hidden">
+              {draftLetters.map((draft) => (
                 <div
                   className="text-gray-80 body-m flex h-10 w-full items-center justify-between gap-1 rounded-lg bg-white p-3"
-                  key={draft.id}
+                  key={draft.letterId}
+                  // onClick={() => handleNavigation(draft.letterId)}
                 >
                   <p className="truncate">{draft.title}</p>
                   <div className="text-gray-20">
