62d67cd5234fe5fe6e6ed17a6b36485ad0f838c9
wldnjs990 <139528356+wldnjs990@users.noreply.github.com>
2025-03-07 22:29:31 +0900

    feat : 재사용 가능한 페이지네이션 구현 (#92)

---
+++ src/pages/Admin/components/PagenationNavigation.tsx
@@ -0,0 +1,93 @@
+import { useState } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+interface PagenationNavigation {
+  totalPage: number;
+  buttonLength: number;
+  handlePageNumberButtonClick: (page: string) => void;
+}
+export default function PagenationNavigation({
+  totalPage,
+  buttonLength,
+  handlePageNumberButtonClick,
+}: PagenationNavigation) {
+  const totalSection = Math.ceil(totalPage / buttonLength) - 1;
+  const [nowSection, setNowSection] = useState<number>(0);
+  const [nowPageNumberAt, setNowPageNumberAt] = useState(1);
+
+  // 네비게이션 시작점, 끝점
+  const navigationRange = {
+    start: nowSection * buttonLength + 1,
+    end: nowSection * buttonLength + buttonLength,
+  };
+
+  // 페이지 버튼 배열
+  const pageNumberButtonArray = Array.from(
+    { length: navigationRange.end - navigationRange.start + 1 },
+    (_, index) => navigationRange.start + index,
+  );
+
+  // 페이지 버튼 클릭시 해당 번호값이 파라미터에 담김
+  const handlePageButtonClick = (page: number) => {
+    const pageString = page.toString();
+    handlePageNumberButtonClick(pageString);
+    setNowPageNumberAt(page);
+  };
+
+  const handlePrevButtonClick = () => {
+    if (nowSection > 0) {
+      const prev = (nowSection - 1) * buttonLength + buttonLength;
+      setNowSection((cur) => cur - 1);
+      handlePageButtonClick(prev);
+    }
+  };
+
+  const handleNextButtonClick = () => {
+    if (nowSection < totalSection) {
+      const next = (nowSection + 1) * buttonLength + 1;
+      setNowSection((cur) => cur + 1);
+      handlePageButtonClick(next);
+    }
+  };
+
+  const buttonStyle = 'border bg-white px-2 py-1 disabled:bg-gray-20';
+
+  return (
+    <div className="mt-5 flex h-10 w-full items-center justify-center">
+      <div className="flex items-center">
+        <button
+          className={twMerge(buttonStyle)}
+          disabled={nowSection <= 0}
+          onClick={() => {
+            handlePrevButtonClick();
+          }}
+        >
+          prev
+        </button>
+        {pageNumberButtonArray.map((num) => {
+          if (totalPage < num) return null;
+          return (
+            <button
+              key={num}
+              className={twMerge(buttonStyle, nowPageNumberAt === num && 'bg-accent-3')}
+              onClick={() => {
+                handlePageButtonClick(num);
+              }}
+            >
+              {num}
+            </button>
+          );
+        })}
+        <button
+          className={twMerge(buttonStyle)}
+          disabled={nowSection >= totalSection}
+          onClick={() => {
+            handleNextButtonClick();
+          }}
+        >
+          next
+        </button>
+      </div>
+    </div>
+  );
+}
