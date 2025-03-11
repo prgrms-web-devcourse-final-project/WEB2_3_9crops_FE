98338cfa4b2159c2d1a62a30e87feb7f5c3131fc
안지원 <wl990@naver.com>
2025-03-11 21:52:11 +0900

    chore : App.tsx에 호출되고 있는 SSE구독 제거

--- src/App.tsx
+++ src/App.tsx
@@ -28,12 +28,10 @@ import RollingPaperPage from './pages/RollingPaper';
 import WritePage from './pages/Write';
 import ShareApprovalPage from './pages/Share';
 import useThemeStore from './stores/themeStore';
-import { useServerSentEvents } from './hooks/useServerSentEvents';
 
 const App = () => {
   const theme = useThemeStore((state) => state.theme);
   useViewport();
-  useServerSentEvents();
 
   const initializeTheme = () => {
     if (theme === 'dark') {
