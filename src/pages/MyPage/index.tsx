d71eb50bc798e77d8d9059652ab2d0577baa008a
Seungyeon Han <tiffanyhansy@gmail.com>
2025-03-11 09:18:34 +0900

    fix: 마이페이지 신고 안내 모달 텍스트 수정

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -70,6 +70,11 @@ const MyPage = () => {
           <article className="bg-accent-1 relative w-77 overflow-hidden rounded-sm p-6">
             <div className="absolute inset-0 h-full w-full bg-white/90 blur-[25px]" />
             <div className="relative">
+              <h2 className="body-sb mb-1.5 text-gray-100">경고 안내</h2>
+              <p className="caption-r mb-5 text-black">
+                따사로운 서비스 이용을 위해, 부적절하다고 판단되는 편지는 반려하고 있어요. 서로를
+                존중하는 따뜻한 공간을 만들기 위해 협조 부탁드립니다.
+              </p>
               <h2 className="body-sb mb-1.5 text-gray-100">경고 규칙</h2>
               <p className="caption-r text-black">3회 경고: 서비스 이용 불가능</p>
             </div>
