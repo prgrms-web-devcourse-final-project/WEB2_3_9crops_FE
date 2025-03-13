9873e0a916388d46f7e74c14f5c8689cb7150d59
wl990@naver.com <wl990@naver.com>
2025-03-14 04:08:14 +0900

    fix : 롤링페이퍼 최대 글자수 500자 제한

--- src/components/TextareaField.tsx
+++ src/components/TextareaField.tsx
@@ -4,6 +4,7 @@ const TextareaField = ({ ...props }: ComponentPropsWithoutRef<'textarea'>) => {
   return (
     <div className="relative z-1 w-full rounded-sm bg-white px-3 py-1.5">
       <textarea
+        maxLength={500}
         className="body-m placeholder:text-gray-30 text-gray-80 w-full resize-none bg-transparent"
         {...props}
       />
